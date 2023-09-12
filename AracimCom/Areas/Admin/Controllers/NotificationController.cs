71c40b5457a3b5d2f3b7d6607207644f3c233b2d
Samet Yucel <sametycl02@gmail.com>
2023-09-13 01:52:52 +0300

    Admin/Notification listed,added,updated and status

--- AracimCom/Areas/Admin/Controllers/NotificationController.cs
+++ AracimCom/Areas/Admin/Controllers/NotificationController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concreate;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntitiyLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using X.PagedList;
 
@@ -14,5 +17,107 @@ namespace AracimCom.Areas.Admin.Controllers
             var values = nm.GetListAll().OrderByDescending(x => x.NotificationID).ToPagedList(page, 5);
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult AddNotification()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNotification(Notification p)
+        {
+
+            NotificationValidator nv = new NotificationValidator();
+            ValidationResult results = nv.Validate(p);
+            if (results.IsValid)
+            {
+                if (p.NotificationTypeSymbol == null && p.NotificationColor == null)
+                {
+                    p.NotificationTypeSymbol = "fas fa-file ";
+                    p.NotificationColor = "blue";
+
+                }
+                else if (p.NotificationTypeSymbol == null)
+                {
+                    p.NotificationTypeSymbol = "fas fa-file ";
+
+                }
+                else if (p.NotificationColor == null)
+                {
+                    p.NotificationColor = "blue";
+                }
+
+                p.NotificationStatus = true;
+                p.NotificationDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                nm.TAdd(p);
+                return RedirectToAction("Index", "Notification");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult UpdateNotification(int id)
+        {
+            var value = nm.GetById(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateNotification(Notification p)
+        {
+            if (p.NotificationTypeSymbol == null && p.NotificationColor == null)
+            {
+                p.NotificationTypeSymbol = "fas fa-file ";
+                p.NotificationColor = "blue";
+
+            }
+            else if (p.NotificationTypeSymbol == null)
+            {
+                p.NotificationTypeSymbol = "fas fa-file ";
+
+            }
+            else if (p.NotificationColor == null)
+            {
+                p.NotificationColor = "blue";
+            }
+
+            p.NotificationStatus = true;
+            p.NotificationDate = DateTime.Parse(DateTime.Now.ToShortDateString());           
+            nm.TUpdate(p);
+            return RedirectToAction("Index", "Notification");
+        }
+
+
+        public IActionResult PassiveNotification(int id)
+        {
+            var x = nm.GetById(id);
+            x.NotificationStatus = false;
+            nm.TUpdate(x);
+            return RedirectToAction("Index", "Notification");
+        }
+
+        public IActionResult ActiveNotification(int id)
+        {
+            var x = nm.GetById(id);
+            x.NotificationStatus = true;
+            nm.TUpdate(x);
+            return RedirectToAction("Index", "Notification");
+        }
+
+        public IActionResult DeleteNotification(int id)
+        {
+            var notificationValue = nm.GetById(id);
+            nm.TDelete(notificationValue);
+            return RedirectToAction("Index", "Notification");
+        }
+
     }
 }
---
+++ BusinessLayer/ValidationRules/NotificationValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntitiyLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class NotificationValidator : AbstractValidator<Notification>
+    {
+        public NotificationValidator()
+        {
+            RuleFor(x => x.NotificationType).NotEmpty().WithMessage("Bildirim Türü  boş geçilemez");
+            RuleFor(x => x.NotificationType).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter giriniz");
+
+        }
+    }
+}
