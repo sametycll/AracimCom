015c1a1f11e6f2ba0f3c4c3e1003fddd64525975
Samet Yucel <sametycl02@gmail.com>
2023-09-13 02:28:38 +0300

    Admin/Navbar Notification and Message

---
+++ AracimCom/Areas/Admin/ViewComponents/Admin/AdminMessageNotification.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AracimCom.ViewComponents.Admin
+{
+    public class AdminMessageNotification:ViewComponent
+    {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        public IViewComponentResult Invoke()
+        {
+            var value = mm.GetListMessageAllWithUsers().OrderByDescending(x => x.MessageID).Take(3).ToList();
+            return View(value);
+        }
+    }
+}
---
+++ AracimCom/Areas/Admin/ViewComponents/Admin/AdminNotification.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AracimCom.ViewComponents.Admin
+{
+    public class AdminNotification : ViewComponent
+    {
+        NotificationManager nm = new NotificationManager(new EfNotificationRepository());
+        public IViewComponentResult Invoke()
+        {
+            var value = nm.GetListAll().OrderByDescending(x => x.NotificationID).Take(3).ToList();
+            return View(value);
+        }
+    }
+}
