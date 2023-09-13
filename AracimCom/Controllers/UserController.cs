81a1b8664ebb429f81e373b736403bc5412ede14
Samet Yucel <sametycl02@gmail.com>
2023-09-13 03:45:03 +0300

    Home/ Vehicle User Detail Page

---
+++ AracimCom/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace AracimCom.Controllers
+{
+    [AllowAnonymous]
+    public class UserController : Controller
+    {
+        VehicleManager vm = new VehicleManager(new EfVehicleRepository());
+        public IActionResult Index(int id, int page = 1)
+        {
+            ViewBag.Id = id;    
+            var values = vm.GetListVehicleWithCategoryUser(id).ToPagedList(page, 10); ;
+            return View(values);
+        }
+    }
+}
---
+++ AracimCom/ViewComponents/User/UserDetails.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using EntitiyLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AracimCom.ViewComponents.User
+{
+    public class UserDetails : ViewComponent
+    {
+        UserManager um = new UserManager(new EfUserRepository());
+        public IViewComponentResult Invoke(int id)
+        {            
+            var values = um.GetById(id);
+            return View(values);
+        }
+    }
+}
